056472064f1a7d65643c41c1812d1d43d2254a6a
Dongmin Shin <dshin2@novetta.com>
2022-07-04 16:38:16 -0400

    first commit

--- src/AI/AiMain.js
+++ src/AI/AiMain.js
@@ -1,21 +1,61 @@
+import Button from '@mui/material/Button';
 import React, {useState} from 'react';
 import './AiMain.css'
+import { makeStyles } from '@mui/styles';
 
-// const useStyles = makeStyles({
-//     root: {
-//         // display: 'flex',
-//         // backgroundColor: 'red',
-//     }
-// })
+const useStyles = makeStyles({
+    root: {
+        // display: 'flex',
+        // backgroundColor: 'red',
+    },
+    buttonContainer: {
+      color: 'red',
+      backgroundColor: 'white',
+    },
+    button: {
+        color: 'red',
+    },
+})
+const createDefaultTabl=(size)=>{
+    const arrCol = [];
+    for (let i = 0; i < size; i++) {
+        const arrRow = [];
+        for (let j = 0; j < size; j++) {
+            arrRow.push({
+                status: false,
+                row: j,
+            });
+        }
+        arrCol.push({
+            col: i,
+            rowArr: arrRow
+        });
+    }
+    return arrCol;
+};
 
 const AiMain = () => {
-    // const classes = useStyles();
-    
+    const [table, setTable] = useState(createDefaultTabl(10));
+    const classes = useStyles();
+
+    const createButton = (m) =>{
+        console.log(m);
+        // onClick={setTable(...table, table[col.col][row.row].status = true)}
+        return (
+            <div className={classes.buttonContainer}>
+                {m.rowArr.map( li => (<Button className={classes.button}>{li.row}</Button>))}
+            </div>
+        );
+    }
     return (
         <div className="root">
-
+            <div>
+            {table.map( m => createButton(m))}
+            </div>
         </div>
     )
 }
+
+
 
 export default AiMain;
